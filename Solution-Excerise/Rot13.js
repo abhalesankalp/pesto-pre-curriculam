9a36817773ebe3691b63924f46c1a99c6d5cf0d0
Sankalp Abhale <Sankalp.Abhale@mindbodyonline.com>
2020-03-13 21:49:40 +0530

    made this funtion generic with rotation count as input

--- Solution-Excerise/Rot13.js
+++ Solution-Excerise/Rot13.js
@@ -1,10 +1,10 @@
-DecodeRotThriteen = (encodedString) =>
+DecodeRotX = (encodedString,number) =>
 {
     var chars = Array.from(encodedString);
     var decodedWords =  chars.map(function(char){
         if(!/^[a-zA-Z0-9]*$/.test(char) )
          return char;
-         var charCode = char.charCodeAt(0) - 13;
+         var charCode = char.charCodeAt(0) - number;
          if(charCode < 65)
             return String.fromCharCode(90-charCode+1);
          else
